c55c189c2adae53c2dfd94c54b49ac837ad0330b
shrikant <s.adhav@easternenterprise.com>
2019-01-17 13:43:42 +0530

    Small fix

--- src/app/modules/app-server-module/app.server.module.ts
+++ src/app/modules/app-server-module/app.server.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
+import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
 import { AppModule } from '../app-module/app.module';
@@ -10,7 +10,8 @@ import { AppComponent } from './../../app-component/app.component';
     // by the ServerModule from @angular/platform-server.
     AppModule,
     ServerModule,
-    ModuleMapLoaderModule // <-- *Important* to have lazy-loaded routes work
+    ModuleMapLoaderModule, // <-- *Important* to have lazy-loaded routes work
+    // ServerTransferStateModule // Freaking Awesome!! 😮 👌 https://medium.com/@hamedbaatour/angular-5-server-side-rendering-with-firebase-step-by-step-guide-ec5f4d8ef38b
   ],
   // Since the bootstrapped component is not inherited from your
   // imported AppModule, it needs to be repeated here.
